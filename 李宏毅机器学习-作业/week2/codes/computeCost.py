d20decb9842ebff9f190a104146b0aeab9210936
446734796@qq.com <446734796@qq.com>
2019-03-19 11:28:55 +0800

    add week2 sample codes

---
+++ 李宏毅机器学习-作业/week2/codes/computeCost.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def compute_cost(X, y, theta):
+    # Initialize some useful values
+    m = y.size
+    cost = 0
+
+    # ===================== Your Code Here =====================
+    # Instructions : Compute the cost of a particular choice of theta.
+    #                You should set the variable "cost" to the correct value.
+
+    cost = np.sum((np.dot(X, theta) - y) ** 2) / (2 * m)
+
+    return cost
